86f98e7aca7521db95df87ef2321a1936b170bc3
HwangInUng <ung6860@naver.com>
2023-09-29 18:08:41 +0900

    refactor: global 예외처리 로직 수정 -ErrorResponse 단순화 -GlobalExceptionHandler 비즈니스 예외 처리 로직 추가

--- src/main/java/com/side/cakeproject/exception/ErrorResponse.java
+++ src/main/java/com/side/cakeproject/exception/ErrorResponse.java
@@ -17,70 +17,11 @@ import java.util.stream.Collectors;
 public class ErrorResponse {
     private HttpStatus status;
     private String message;
-    private List<FieldError> errors;
-    private String reason;
 
     @Builder
-    public ErrorResponse(final ErrorCode errorCode) {
-        this.status = errorCode.getStatus();
-        this.message = errorCode.getMessage();
-        this.errors = new ArrayList<>();
+    public ErrorResponse(final CakeException exception) {
+        this.status = exception.getStatus();
+        this.message = exception.getMessage();
     }
 
-    @Builder
-    public ErrorResponse(final ErrorCode errorCode, final String reason) {
-        this.status = errorCode.getStatus();
-        this.message = errorCode.getMessage();
-        this.reason = reason;
-    }
-
-    @Builder
-    public ErrorResponse(final ErrorCode errorCode, final List<FieldError> errors) {
-        this.status = errorCode.getStatus();
-        this.message = errorCode.getMessage();
-        this.errors = errors;
-    }
-
-    public static ErrorResponse of(final ErrorCode errorCode, final BindingResult bindingResult) {
-        return new ErrorResponse(errorCode, FieldError.of(bindingResult));
-    }
-
-    public static ErrorResponse of(final ErrorCode errorCode) {
-        return new ErrorResponse(errorCode);
-    }
-
-    public static ErrorResponse of(final ErrorCode errorCode, final String reason) {
-        return new ErrorResponse(errorCode, reason);
-    }
-
-    @Getter
-    public static class FieldError {
-        private final String field;
-        private final String value;
-        private final String reason;
-
-        public static List<FieldError> of(final String field, final String value, final String reason) {
-            List<FieldError> fieldErrors = new ArrayList<>();
-            fieldErrors.add(new FieldError(field, value, reason));
-            return fieldErrors;
-        }
-
-        public static List<FieldError> of(final BindingResult bindingResult) {
-            final List<org.springframework.validation.FieldError> fieldErrors = bindingResult.getFieldErrors();
-            return fieldErrors.stream()
-                    .map(error -> new FieldError(
-                            error.getField(),
-                            error.getRejectedValue() == null ? "" : error.getRejectedValue().toString(),
-                            error.getDefaultMessage()
-                    ))
-                    .collect(Collectors.toList());
-        }
-
-        @Builder
-        FieldError(String field, String value, String reason) {
-            this.field = field;
-            this.value = value;
-            this.reason = reason;
-        }
-    }
 }
--- src/main/java/com/side/cakeproject/exception/GlobalExceptionHandler.java
+++ src/main/java/com/side/cakeproject/exception/GlobalExceptionHandler.java
@@ -1,10 +1,17 @@
 package com.side.cakeproject.exception;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CakeException.class)
+    protected ResponseEntity<ErrorResponse> handleCakeException(CakeException e) {
+        final ErrorResponse errorResponse = new ErrorResponse(e);
+        return ResponseEntity.status(e.getStatus()).body(errorResponse);
+    }
 }
